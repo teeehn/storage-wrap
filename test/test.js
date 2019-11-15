2ae639f0e8f2a3eb6ce495b8d861e798a1339624
Thomas Nicolosi <Thomas.Nicolosi@panasonic.aero>
2019-11-15 10:36:03 -0800

    Got localStorage mock to work.

--- test/test.js
+++ test/test.js
@@ -1,15 +1,31 @@
 const assert = require("chai").assert;
 const StorageWrap =  require("../dist/storage-wrap.cjs");
-const window = global;
+
+// Mock localStorage
+global.window = {};
+require("mock-local-storage");
+window.localStorage = global.localStorage;
 
 describe("StorageWrap", function () {
     it("typeof StorageWrap is an object", function () {
         assert.typeOf(StorageWrap, "object");
     });
-    it("typeof StorageWrap.read_storage is function", function() {
-        assert.typeOf(StorageWrap.storage_read, "function");
-    });
     it("localStorage is neither null nor undefined", function () {
-        assert.exists(localStorage);
+        assert.exists(window.localStorage);
+    });
+    describe("StorageWrap.storage_read", function () {
+
+        const { 
+            storage_read,
+            storage_write
+        } = StorageWrap;
+
+        it("typeof storage_read is function", function() {
+            assert.typeOf(storage_read, "function");
+        });
+
+        it("storage_read with key 'test' is null", function () {
+            assert.isNull(storage_read('test'));
+        });
     });
 });
